e3f1b2e50d2ec2a76b60477bf812692355804a83
Sophie Au <SophieAu@users.noreply.github.com>
2021-04-02 20:46:18 +0200

    Test Uninstall

--- backend/Uninstall_test.go
+++ backend/Uninstall_test.go
@@ -1,36 +1,85 @@
 package backend_test
 
 import (
+	"errors"
 	"imperial-splendour-launcher/backend/testHelpers"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	testifyMock "github.com/stretchr/testify/mock"
 
 	"testing"
 )
 
 func TestUninstall(t *testing.T) {
-	TestSuccessfullyUninstallImpSplen := func(t *testing.T) {
-		fileList := "merp.pack\nz.lua"
-
+	t.Run("Cannot deactivate and therefore uninstall Imperial Splendour", func(t *testing.T) {
+		// NOTE: Deactivating Imperial Splendour is tested more thoroughly in the Switch_test.go file
 		api, _, _, _, sysHandler := testHelpers.VariableBefore("2.0", true, "test")
-		sysHandler.On("ReadFile", "./IS_Files/IS_FileList.txt").Return([]byte(fileList), nil).Once()
+		sysHandler.On("ReadFile", "./IS_Files/IS_FileList.txt").Return(nil, errors.New("Error Deactivating")).Once()
+
+		err := api.Uninstall()
+
+		assert.Equal(t, "Could not uninstall", err.Error())
+		sysHandler.AssertNotCalled(t, "Remove", testifyMock.Anything)
+
+		testHelpers.After(*api)
+	})
+
+	t.Run("Can deactivate but cannot uninstall Imperial Splendour", func(t *testing.T) {
+		api, _, _, _, sysHandler := testHelpers.VariableBefore("2.0", true, "test")
+		sysHandler.On("ReadFile", "./IS_Files/IS_FileList.txt").Return([]byte("merp.pack"), nil)
 		sysHandler.On("MoveFile", testifyMock.Anything, testifyMock.Anything).Return(nil)
-		sysHandler.On("WriteFile", "./IS_Files/IS_info.json", testHelpers.FmtInfoFile(false, "2.0", "test")).Return(nil)
+		sysHandler.On("WriteFile", "./IS_Files/IS_info.json", mock.Anything).Return(nil)
+		sysHandler.On("Remove", testifyMock.Anything, testifyMock.Anything).Return(errors.New("Could not delete files"))
+
+		err := api.Uninstall()
+
+		assert.Equal(t, "Could not delete files", err.Error())
+		sysHandler.AssertCalled(t, "MoveFile", "./data/merp.pack", "./IS_Files/merp.pack")
+		sysHandler.AssertCalled(t, "Remove", "./IS_Files/")
+
+		testHelpers.After(*api)
+	})
+
+	t.Run("Deactivates and uninstalls Imperial Splendour", func(t *testing.T) {
+		api, _, _, _, sysHandler := testHelpers.VariableBefore("2.0", true, "test")
+		sysHandler.On("ReadFile", "./IS_Files/IS_FileList.txt").Return([]byte("merp.pack"), nil)
+		sysHandler.On("MoveFile", testifyMock.Anything, testifyMock.Anything).Return(nil)
+		sysHandler.On("WriteFile", "./IS_Files/IS_info.json", mock.Anything).Return(nil)
 		sysHandler.On("Remove", testifyMock.Anything, testifyMock.Anything).Return(nil)
 
 		err := api.Uninstall()
 
 		assert.Nil(t, err)
-		// deactivating
 		sysHandler.AssertCalled(t, "MoveFile", "./data/merp.pack", "./IS_Files/merp.pack")
-		sysHandler.AssertCalled(t, "MoveFile", "./data/campaigns/imperial_splendour/z.lua", "./IS_Files/z.lua")
-		sysHandler.AssertCalled(t, "MoveFile", "APPDATA/The Creative Assembly/Empire/scripts/user.empire_script.txt", "./IS_Files/user.empire_script.txt")
-		sysHandler.AssertCalled(t, "WriteFile", "./IS_Files/IS_info.json", testHelpers.FmtInfoFile(false, "2.0", "test"))
-		//deleting
 		sysHandler.AssertCalled(t, "Remove", "./IS_Files/")
 
 		testHelpers.After(*api)
-	}
-	TestSuccessfullyUninstallImpSplen(t)
+	})
+
+	t.Run("Doesn't need to deactivate Imperial Splendour but cannot uninstall it", func(t *testing.T) {
+		api, _, _, _, sysHandler := testHelpers.VariableBefore("2.0", false, "test")
+		sysHandler.On("Remove", testifyMock.Anything, testifyMock.Anything).Return(errors.New("Could not delete files"))
+
+		err := api.Uninstall()
+
+		assert.Equal(t, "Could not delete files", err.Error())
+		sysHandler.AssertNotCalled(t, "MoveFile", testifyMock.Anything, testifyMock.Anything)
+		sysHandler.AssertCalled(t, "Remove", "./IS_Files/")
+
+		testHelpers.After(*api)
+	})
+
+	t.Run("Doesn't need to deactivate Imperial Splendour and uninstalls it", func(t *testing.T) {
+		api, _, _, _, sysHandler := testHelpers.VariableBefore("2.0", false, "test")
+		sysHandler.On("Remove", testifyMock.Anything, testifyMock.Anything).Return(nil)
+
+		err := api.Uninstall()
+
+		assert.Nil(t, err)
+		sysHandler.AssertNotCalled(t, "MoveFile", testifyMock.Anything, testifyMock.Anything)
+		sysHandler.AssertCalled(t, "Remove", "./IS_Files/")
+
+		testHelpers.After(*api)
+	})
 }
