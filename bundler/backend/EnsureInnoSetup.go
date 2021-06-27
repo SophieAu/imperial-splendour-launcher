75bc5738f269854fcb3a88afc29d0cee4c78cf7d
Sophie Au <SophieAu@users.noreply.github.com>
2021-06-27 18:43:45 +0200

    Windows debugging

--- bundler/backend/EnsureInnoSetup.go
+++ bundler/backend/EnsureInnoSetup.go
@@ -2,10 +2,18 @@ package backend
 
 import (
 	"imperial-splendour-bundler/backend/customErrors"
+	"os/exec"
 )
 
 func (a *API) EnsureInnoSetup() error {
-	err := a.Sh.StartCommand("iscc /?")
+
+	path, err := exec.LookPath("iscc")
+	if err != nil {
+		a.logger.Warn("installing fortune is in your future")
+	}
+	a.logger.Infof("fortune is available at %s\n", path)
+
+	err = a.Sh.StartCommand("iscc /?")
 	if err != nil {
 		return a.error("InnoSetup not installed", customErrors.InnoSetup)
 	}
